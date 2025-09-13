90befb30ab62bc00b856f3a536901212b70d7210
ryemonk <ry_e@hotmail.com>
2025-09-12 22:51:49 -0600

    Create index.js

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: "sans-serif", padding: "2rem" }}>
+      <h1>AI Career Pathfinder</h1>
+      <p>Welcome Rye! 🚀</p>
+      <p>This will help you explore careers that are future-proof and in demand.</p>
+    </div>
+  );
+}
